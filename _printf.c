eb988c55d6fdd597cdd9facea4e959ae16bf006b
codivily <codivily@gmail.com>
2022-04-17 14:11:31 -0700

    _printf function definitation added

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+/**
+ * _printf - produces output according to a format.
+ * @format: A string
+ *
+ * Return: the of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	return (0);
+}
