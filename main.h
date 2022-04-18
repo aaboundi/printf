d46f32b391b0e0401ab2e87258c193245644d1e9
aaboundi <aadja@gmail.com>
2022-04-18 05:22:39 -0700

    Main

--- main.h
+++ main.h
@@ -8,4 +8,21 @@ int _putchar(char c);
 void _print_string(const char *s);
 int _printf(const char *format, ...);
 
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/* get_flag */
+int get_flag(char s, flags_t *f);
+
 #endif /* MAIN_H */
