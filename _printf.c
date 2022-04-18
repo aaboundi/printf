6b1feb89709f5fce6a1d3c98e4e97ee7de2b9e96
codivily <codivily@gmail.com>
2022-04-18 04:24:37 -0700

    _printf_c TODOs added

--- _printf.c
+++ _printf.c
@@ -9,5 +9,32 @@
  */
 int _printf(const char *format, ...)
 {
-	return (0);
+	va_list args;
+	unsigned int i = 0, n_printed = 0;
+	char current_char = '\0', next_char = '\0';
+
+	va_start(args, format);
+
+	if (format != NULL)
+	{
+		while (format[i] != '\0')
+		{
+			current_char = format[i];
+
+			/* TODO: check if current_char is a % sign */
+			/* TODO: if current_char is a % sign */
+			/*       (a) check if set next_char */
+			/*       (b) check if next_char is %, s or c */
+			/*           - if yes -> call va_arg and print */
+			/*           - if no  -> skip */
+
+			/* TODO: if current_char is not a % sign, print */
+
+			i++;
+		}
+	}
+
+	va_end(args);
+
+	return (n_printed);
 }
