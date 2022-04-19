317bfd9300dbfa2176b0e4e5b6c49e117e3bc823
codivily <codivily@gmail.com>
2022-04-19 11:16:19 -0700

    _print_int, _print_int_arg added

--- main.h
+++ main.h
@@ -24,5 +24,7 @@ int _printf(const char *format, ...);
 int print_str(va_list args);
 int print_char(va_list args);
 int print_pct(va_list args);
+int _print_int(int n);
+int _print_int_arg(va_list args);
 
 #endif /* MAIN_H */
